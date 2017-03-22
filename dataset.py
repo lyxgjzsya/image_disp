e4fcc0a19b3b76bec9349710f16ea0d4b70c8877
luoyaox <luoyaox@qq.com>
2017-03-22 15:37:40 +0800

    first version

---
+++ dataset.py
@@ -0,0 +1,129 @@
+# -*- coding: UTF-8 -*-
+import numpy as np
+import os
+from PIL import Image
+
+EPIWidth = 16 #还没有关联到network的构建，所以不要直接在这里改EPIwidth
+
+class Dataset(object):
+
+    def __init__(self,images,labels,reshape=True):
+        assert images.shape[0]==labels.shape[0], (
+            'images.shape: %s labels.shape: %s' % (images.shape, labels.shape))
+        self._num_examples=images.shape[0]
+        print ('num of example:%d' % (self._num_examples))
+        if reshape:
+            images = images.reshape(images.shape[0],
+                                    images.shape[1]*images.shape[2]*images.shape[3])
+
+#        images=images.astype(np.float32)
+#        images=np.multiply(images,1.0/255.0)
+
+        self._images=images
+        self._labels=labels
+        self._epochs_completed = 0
+        self._index_in_epoch = 0
+
+    @property
+    def images(self):
+        return self._images
+
+    @property
+    def labels(self):
+        return self._labels
+
+    @property
+    def num_examples(self):
+        return self._num_examples
+
+    @property
+    def epochs_completed(self):
+        return self._epochs_completed
+
+    def next_batch(self,batch_size=1):
+        '''
+        get next batch
+        :param batch_size:
+        :return: next data&label batch
+        '''
+        start = self._index_in_epoch
+        self._index_in_epoch += batch_size
+        if self._index_in_epoch > self._num_examples:#如果大于一次循环，重新shuffle一次
+            perm = np.arange(self._num_examples)
+            np.random.shuffle(perm)
+            self._images = self._images[perm]
+            self._labels = self._labels[perm]
+            start = 0
+            self._index_in_epoch = batch_size
+            assert batch_size <= self._num_examples
+        end = self._index_in_epoch
+
+        return self._images[start:end], self._labels[start:end]
+
+
+def EPIextractor(image):
+    '''
+    turn big EPI to small EPI
+    :param image:np.array[9,512,3] EPI
+    :return:list[512,9,EPIwidth,3]
+    '''
+    height = image.shape[0]
+    width = image.shape[1]
+    paddinghead = image[:,range(8,0,-1),:]#左右颠倒
+    paddinghead = paddinghead[range(8,-1,-1),:,:] #上下颠倒
+    paddingtail = image[:,range(width-2,width-2-EPIWidth/2,-1),:]
+    paddingtail = paddingtail[range(8,-1,-1),:,:]
+    #在原图最左与最右 添加翻转过的内容作为边界填充 假设取9*16*3卷积 原图就变成9*(16/2+512+16/2)*3
+    image = np.column_stack((paddinghead,image,paddingtail))
+    subEPI = [image[:,i-EPIWidth/2:i+EPIWidth/2,:] for i in range(EPIWidth/2,width+EPIWidth/2)]
+
+    return subEPI
+
+def read_disp(dir):
+    '''
+    read disp.txt
+    :param dir: the path to disp.txt
+    :return: np.array[512*512] image.shape[0]*image.shape[1]
+    '''
+    disp_list = []  # 512*512
+    with open(dir, 'r') as f:
+        for line in f.readlines():
+            line = line.strip()
+            data = line.split(' ')
+            data = map(float, data)
+            disp_list.append(data)
+    disp = np.array(disp_list)
+    disp = disp.reshape(disp.shape[0] * disp.shape[1])
+    return disp
+
+def read_data(dir):
+    '''
+    read EPI and turn it to small EPI
+    :param dir:
+    :return: np.array[(512*512),9,EPIwidth,3]
+    '''
+    files = []
+    datalist = []
+    filelist = os.listdir(dir)
+    for f in filelist:
+        if os.path.isfile(dir+'/'+f):
+            filename = dir+'/'+f
+            if filename.find('.png') != -1:
+                files.append(filename)
+    files.sort()
+    for png_path in files:
+        with open(png_path) as f:
+            im = Image.open(f)
+            subEPI = EPIextractor(np.array(im))
+            datalist.append(subEPI)
+    datas = np.array(datalist)
+    datas = datas.reshape([datas.shape[0]*datas.shape[1],datas.shape[2],datas.shape[3],datas.shape[4]])
+
+    return datas
+
+
+def get_datasets(dir):
+    new_disp = read_disp(dir+'/disp.txt')
+    new_data = read_data(dir+'/pngdata/epi36_44')
+
+    return Dataset(new_data,new_disp)
---
+++ network.py
@@ -0,0 +1,51 @@
+# -*- coding: UTF-8 -*-
+import tensorflow as tf
+
+#测试用小型网络
+def inference(image_pl):
+    image_placeholder = tf.reshape(image_pl,[-1,9,16,3])
+    w_conv1 = tf.Variable(tf.truncated_normal([3, 3, 3, 32], stddev=1e-2))
+    conv1 = tf.nn.conv2d(image_placeholder, w_conv1, [1, 1, 1, 1], padding='SAME')
+    b_conv1 = tf.Variable(tf.constant(0.1, shape=[32]))
+    a_conv1 = tf.nn.relu(tf.nn.bias_add(conv1, b_conv1))
+    pool1 = tf.nn.max_pool(a_conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+    w_conv2 = tf.Variable(tf.truncated_normal([3, 3, 32, 32], stddev=1e-2))
+    conv2 = tf.nn.conv2d(pool1, w_conv2, [1, 1, 1, 1], padding='SAME')
+    b_conv2 = tf.Variable(tf.constant(0.1, shape=[32]))
+    a_conv2 = tf.nn.relu(tf.nn.bias_add(conv2, b_conv2))
+    pool2 = tf.nn.max_pool(a_conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+    w_fc1 = tf.Variable(tf.truncated_normal([3 * 4 * 32, 100], stddev=1e-2))
+    b_fc1 = tf.Variable(tf.constant(0.1, shape=[100]))
+    pool2_tmp = tf.reshape(pool2, [-1, 3 * 4 * 32])
+    a_fc1 = tf.nn.relu(tf.matmul(pool2_tmp, w_fc1) + b_fc1)
+
+    w_fc2 = tf.Variable(tf.truncated_normal([100, 1], stddev=1e-2))
+    b_fc2 = tf.Variable(tf.constant(0.1, shape=[1]))
+    a_fc2 = tf.matmul(a_fc1, w_fc2) + b_fc2
+
+    print 'image:',image_placeholder.get_shape()
+    print 'a_conv1:',a_conv1.get_shape()
+    print 'pool1',pool1.get_shape()
+    print 'a_conv2',a_conv2.get_shape()
+    print 'pool2',pool2.get_shape()
+    print 'a_fc1',a_fc1.get_shape()
+    print 'a_fc2',a_fc2.get_shape()
+
+    return a_fc2
+
+
+def loss(logits, labels_placeholder):
+    Loss = tf.reduce_sum(tf.pow(logits - labels_placeholder, 2))
+
+    return Loss
+
+
+def training(loss, learning_rate, global_step):
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+    train_op = optimizer.minimize(loss, global_step=global_step)
+
+    return train_op
+
+
